86b5afdf336c530bffad71eabe749f1ebe4d0eea
huangyingw@linux_programming <huangyingw@gmail.com>
2012-06-12 13:59:05 -0500

    n

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,5 @@
 public class Test {
 	public static void main(String[] args) {
-		Fibonacci.printFibonacciNumber(0, 1, 10);// print the 20 advanced
-													// fibonacci number
-		Fibonacci.printFibonacciArray(0, 1, 20);
+		Fibonacci.FibonacciLoop(10);
 	}
 }
