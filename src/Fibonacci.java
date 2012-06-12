7ed8055a05a09c667c3431e963d6bb86a8b929f6
huangyingw@linux_programming <huangyingw@gmail.com>
2012-06-12 14:22:19 -0500

    n

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -10,16 +10,28 @@ public class Fibonacci {
 	}
 
 	public int FibonacciRec(int n) {
+		count++;
 		if (n == 0)
 			return 0;
 		else if (n == 1)
 			return 1;
-		else
+		else {
+
 			return FibonacciRec(n - 1) + FibonacciRec(n - 2);
+		}
 	}
 
 	public int[] data;
 	private int size;
+	private int count = 0;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
 
 	public Fibonacci(int size) {
 		super();
@@ -37,9 +49,16 @@ public class Fibonacci {
 			return 0;
 		else if (n == 1)
 			return 1;
-		else
+		else {
 			data[n] = FibonacciRecMemorized(n - 1)
 					+ FibonacciRecMemorized(n - 2);
+			count++;
+		}
 		return data[n];
+	}
+
+	public void resetCount() {
+		// TODO Auto-generated method stub
+		setCount(0);
 	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,16 @@
 public class Test {
 	public static void main(String[] args) {
 		Fibonacci fib = new Fibonacci(10);
+		fib.resetCount();
 		System.out.println(fib.FibonacciLoop(10));
+		System.out.println(fib.getCount());
+
+		fib.resetCount();
 		System.out.println(fib.FibonacciRec(10));
+		System.out.println(fib.getCount());
+
+		fib.resetCount();
 		System.out.println(fib.FibonacciRecMemorized(10));
+		System.out.println(fib.getCount());
 	}
 }
